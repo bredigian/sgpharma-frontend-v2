43da4922c569aec1cf23c1463adfcb854836688d
Gianluca Bredice <bredicegian@gmail.com>
2024-05-09 12:55:52 -0300

    feature: montserrat font configurated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -14,7 +14,12 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      revue: ["Revue", "sans-serif"],
+      qkMarisa: ["QK Marisa", "sans-serif"],
+    },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
