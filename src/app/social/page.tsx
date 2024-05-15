ef3bcd5f8012feaa19fb5d3e41c0cf44262245c7
Gianluca Bredice <bredicegian@gmail.com>
2024-05-15 09:36:01 -0300

    feature: add page hader and carousel section to social page (missing gallery modal)

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,3 +1,20 @@
-export default function Social() {
-  return <main>Social</main>
+import { ISocial } from '@/types/social.types';
+import PageHeader from '@/components/page-header';
+import SocialCarouselSection from '@/sections/social-carousel';
+import { getAll } from '@/services/social.service';
+import socialBackground from '@/assets/background/social.png';
+
+export default async function Social() {
+  const social: ISocial[] = await getAll();
+
+  return (
+    <main className='flex flex-col'>
+      <PageHeader
+        title='Desarrollo social'
+        description='Tenemos un departamento encargado el cual agenda y calendariza actividades como jornadas médicas en hospitales o municipalidades, con lo que hacemos llegar nuestro producto a la población de forma integral.'
+        image={socialBackground}
+      />
+      <SocialCarouselSection data={social} />
+    </main>
+  );
 }
---
+++ src/sections/social-carousel.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+
+import { EmblaCarouselType } from 'embla-carousel';
+import { ISocial } from '@/types/social.types';
+import Image from 'next/image';
+import useEmblaCarousel from 'embla-carousel-react';
+
+type Props = {
+  data: ISocial[];
+};
+
+export default function SocialCarouselSection({ data }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+  const onDotClick = useCallback(
+    (index: number) => {
+      if (!emblaApi) return;
+      emblaApi.scrollTo(index);
+    },
+    [emblaApi],
+  );
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
+  return (
+    <section className='p-8'>
+      <div
+        ref={emblaRef}
+        className='embla relative z-10 mx-auto max-w-[940px] cursor-grab overflow-hidden rounded-xl border-2 border-gray-100 active:cursor-grabbing'
+      >
+        <div className='embla__container flex'>
+          {data.map((item) => (
+            <div
+              key={item.ID}
+              className='embla__slide mx-2 flex shrink-0 grow basis-full flex-col items-center gap-4 p-4 md:flex-row md:items-start'
+            >
+              <Image
+                alt={`Imágen de ${item.ID}`}
+                src={item.IMAGEN_PRINCIPAL}
+                width={1000}
+                height={1000}
+                className='w-full max-w-72 rounded-xl'
+              />
+              <div className='flex flex-col items-center gap-4 md:items-start'>
+                <span className='text-center text-xs font-semibold text-black md:text-start md:text-sm lg:text-lg xl:text-xl'>
+                  {item.TITULO}
+                </span>
+                <p className='text-justify text-[10px] text-gray-200 md:text-sm lg:text-base'>
+                  {item.DESCRIPCION}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
