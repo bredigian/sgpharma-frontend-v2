73e00ff09e0ecf3f2d98839ff2098db3f1f5623d
Gianluca Bredice <bredicegian@gmail.com>
2024-05-15 18:35:51 -0300

    feature: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,10 @@
+import PageHeader from '@/components/page-header';
+import aboutBackground from '@/assets/background/about.png';
+
 export default function NotFound() {
-  return <main>Not found</main>
+  return (
+    <main className='flex flex-col'>
+      <PageHeader title='Página no encontrada' image={aboutBackground} />
+    </main>
+  );
 }
